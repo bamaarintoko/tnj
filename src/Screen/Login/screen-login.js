3c1b5465e08f6a7050e8fb71c3ee0c4ca725e8f3
bama <yodafatbama.arintko@gmail.com>
2018-11-24 00:05:58 +0700

    asd

--- src/Screen/Login/screen-login.js
+++ src/Screen/Login/screen-login.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet, StatusBar, Image} from 'react-native'
+import {Container, Input, Item, Button} from 'native-base'
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 function mapStateToProps(state) {
     return {
-        redAuth : state.redAuth
+        redAuth: state.redAuth
     };
 }
 
@@ -16,13 +18,84 @@ class ScreenLogin extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Login</Text>
-            </View>
+            <Container>
+                <StatusBar backgroundColor={"#FF6E40"}/>
+                <View style={styles.container}>
+                <Text style={{fontSize:hp('3%'), color:'#FFF'}}>Tonjoo Test</Text>
+                </View>
+                <View style={styles.container_}>
+                    <View style={{width: '100%'}}>
+                        <View style={styles.input}>
+                            <Text style={styles.label_text}>Username</Text>
+                            <Item regular style={styles.text_input}>
+                                <Input/>
+                            </Item>
+                        </View>
+                        <View style={styles.input}>
+                            <Text style={styles.label_text}>Password</Text>
+                            <Item regular style={styles.text_input}>
+                                <Input/>
+                            </Item>
+                        </View>
+                        <View style={styles.btn}>
+                            <Button full success style={{backgroundColor:'#FF6E40'}}>
+                                <Text style={{color:'#FFF'}}>Login</Text>
+                            </Button>
+                        </View>
+                    </View>
+                </View>
+            </Container>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    input: {
+        marginLeft: 15,
+        marginRight: 15,
+        marginBottom: 10
+    },
+    btn:{
+        marginLeft: 15,
+        marginRight: 15,
+        marginBottom: 10,
+        marginTop: 40
+    },
+    label_text: {
+        color: '#FFF',
+        fontSize: hp('2.5%'),
+        marginBottom:3
+    },
+    text_input: {
+        backgroundColor: '#FFF',
+        borderColor:'#FFF',
+        height: hp('6%')
+    },
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f05638',
+        height: '100%',
+        flex: 1
+    },
+    container_: {
+        flexDirection: 'column',
+        backgroundColor: '#f05638',
+        height: '100%',
+        flex: 2
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    }
+});
 export default connect(
     mapStateToProps,
 )(ScreenLogin);
--- src/Screen/Splash/screen-splash.js
+++ src/Screen/Splash/screen-splash.js
@@ -1,28 +1,61 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {View, Text} from "react-native"
+import {View, Text, StyleSheet,Image, StatusBar} from "react-native"
+import {Container, Content} from 'native-base'
+import { StackActions, NavigationActions } from 'react-navigation';
 function mapStateToProps(state) {
     return {
         state: state.nav,
     };
 }
-
+const resetAction = StackActions.reset({
+    index: 0,
+    actions: [NavigationActions.navigate({ routeName: 'Login' })],
+});
 class ScreenSplash extends Component {
     componentDidMount() {
+        setTimeout(()=>{
+            this.props.navigation.dispatch(resetAction)
+        },3000)
         console.log(this.props.state)
     }
 
     render() {
         return (
-            <View>
-                <Text>
-                    Splash
-                </Text>
-            </View>
+            <Container>
+                <StatusBar backgroundColor={"#FF6E40"}/>
+                <View style={styles.container}>
+                    <Image
+                        style={{flex: 1}}
+                        width={200}
+                        source={require('../../Assets/splash.png')}
+                        resizeMode={"contain"}
+                    />
+                </View>
+
+            </Container>
         );
     }
 }
-
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:'#f05638',
+        height: '100%'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    }
+});
 export default connect(
     mapStateToProps,
 )(ScreenSplash);
